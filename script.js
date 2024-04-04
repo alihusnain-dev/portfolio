c6abae78e1a9faf861de9e3f89f891cfd8762613
HaiderIsBack <haidershahpak17@gmail.com>
2024-04-04 10:25:53 -0700

    Fixed

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ window.onscroll = () => {
 
     let header = document.querySelector('header');
 
-    header.classList.toggle('sticky', windows.scrollY > 100);
+    header.classList.toggle('sticky', window.scrollY > 100);
 
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
